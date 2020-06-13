ea357108e3847e95b5fb7110a2dc12d20dea5134
yaodawei <yaodawei@wxdata.cn>
2020-06-13 10:38:31 +0800

    git 测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>c测试git</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
